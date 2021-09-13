5bf317928524c61d1ec2693d94da05f4979aed76
saintech <4853612+saintech@users.noreply.github.com>
2021-09-13 20:24:10 +0300

    remove all files except play.html

--- cmake/toolchain/darwin-x86_64/README.txt
+++
@@ -1,2 +0,0 @@
-wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz
-tar xJf MacOSX10.15.sdk.tar.xz --strip-components=1
--- tests/queries/0_stateless/01042_h3_k_ring.sql
+++
@@ -1,5 +0,0 @@
--- Tags: no-unbundled, no-fasttest
-
-SELECT arraySort(h3kRing(581276613233082367, 1));
-SELECT h3kRing(581276613233082367, 0);
-SELECT h3kRing(581276613233082367, -1); -- { serverError 12 }
